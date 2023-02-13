92f7077a7948a997577b2ff8573dd69fcc47790d
guillex387 <guillesiglez387@gmail.com>
2023-02-13 22:36:45 +0000

    Adjust the tabs

--- .config/nvim/lua/user/editor.lua
+++ .config/nvim/lua/user/editor.lua
@@ -2,6 +2,7 @@ vim.opt.mouse = ''
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.bo.softtabstop = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.backup = false
