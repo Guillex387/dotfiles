a779087310b2a1157ac3ff209f3dc78c2033c9fb
guillex387 <guillesiglez387@gmail.com>
2023-02-26 20:19:19 +0000

    Solved some neovim issues

--- .config/nvim/lua/user/lualine.lua
+++ .config/nvim/lua/user/lualine.lua
@@ -14,8 +14,8 @@ require('lualine').setup {
   tabline = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { require'tabline'.tabline_buffers },
-    lualine_x = { require'tabline'.tabline_tabs },
+    lualine_c = { require('tabline').tabline_buffers },
+    lualine_x = { require('tabline').tabline_tabs },
     lualine_y = {},
     lualine_z = {},
   },
--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -1,11 +1,17 @@
 local nt_maps = require('user.config.keymaps').nvim_tree
+
 local keymap = function (map, func)
   vim.keymap.set('n', map.key, func, { desc = map.desc })
 end
 
+vim.g.loaded_netrw = 0
+vim.g.loaded_netrwPlugin = 0
+
 require('nvim-tree').setup {
-  hijack_cursor = true,
-  sync_root_with_cwd = true, 
+  disable_netrw = true,
+  sync_root_with_cwd = true,
+  respect_buf_cwd = true,
+  reload_on_bufenter = true,
   update_focused_file = {
     enable = true,
     update_root = true
@@ -13,10 +19,12 @@ require('nvim-tree').setup {
   view = {
     mappings = {
       list = {
-        -- Override default mappings
         { key = nt_maps.cd.key, action = 'cd' }
       }
     }
+  },
+  diagnostics = {
+    enable = true
   }
 }
 
